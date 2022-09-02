879ba32d0dda8587964cc17d5823b826b02417ff
Sai Sasha <sashasai2311@gmail.com>
2022-09-02 17:01:02 +0300

    1 com

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelectorAll(".btn-for-buy");
+
+
+btn.forEach(element => {
+    element.addEventListener("click", f = () =>{
+        window.open('https://www.instagram.com/kalina_p11/');
+    })
+});
